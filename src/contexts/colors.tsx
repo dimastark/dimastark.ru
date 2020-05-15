3e7472d055c9d7e89080e358c094bef5285d6639
Дима Старков <dvstark@yandex.ru>
2020-05-16 01:59:45 +0500

    Add c++ to language list

--- src/contexts/colors.tsx
+++ src/contexts/colors.tsx
@@ -1,13 +1,7 @@
 import { createContext, useContext } from 'react';
 
 export function getPrimaryColor(): string {
-    const choices = [
-        '#F53030',
-        '#673AB7',
-        '#03A9F4',
-        '#4CBF50',
-        '#FF5722'
-    ];
+    const choices = ['#F53030', '#673AB7', '#03A9F4', '#4CBF50', '#FF5722'];
 
     return choices[Math.floor(Math.random() * choices.length)];
 }
@@ -17,7 +11,7 @@ interface IColorsContext {
 }
 
 export const ColorsContext = createContext<IColorsContext>({
-    primary: getPrimaryColor()
+    primary: getPrimaryColor(),
 });
 
 export function useColors(): IColorsContext {
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -14,9 +14,11 @@ export function Main() {
                     I work in <span style={{ color: '#f00' }}>Y</span>andex.
                 </div>
                 <div>
-                    Love&nbsp;to&nbsp;write&nbsp;code&nbsp;on <span style={{ color: '#29beb0' }}>GO</span>,&nbsp;
-                    <span style={{ color: '#8ec653' }}>JS</span>,&nbsp;
-                    <span style={{ color: '#386a92' }}>Python</span>.
+                    Love&nbsp;to&nbsp;write&nbsp;code&nbsp;on
+                    <span style={{ color: '#064481' }}> C++</span>,&nbsp;
+                    <span style={{ color: '#00add8' }}>GO</span>,&nbsp;
+                    <span style={{ color: '#3572a5' }}>Python</span>,&nbsp;
+                    <span style={{ color: '#8ec653' }}>JS</span>.
                 </div>
             </div>
         </Layout>
